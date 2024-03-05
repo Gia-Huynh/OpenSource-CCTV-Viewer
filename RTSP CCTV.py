558437d406c7890a346acedb723c147ddc9a4c2e
GasTheWoods <124686986+Gia-Huynh@users.noreply.github.com>
2024-03-05 17:38:56 +0700

    Scaling Window

--- RTSP CCTV.py
+++ RTSP CCTV.py
@@ -1,21 +1,6 @@
 import cv2
 import numpy as np
-#import face_recognition
-cap = cv2.VideoCapture('rtsp://admin:thietgia95@192.168.1.245:554/cam/realmonitor?channel=1&subtype=1')
 i = 0
-"""while True:
-    ret, img = cap.read()
-    cv2.imshow('video output', img)
-    face_locations = face_recognition.face_locations(img)
-    if (len(face_locations)!=0):
-        print("I found {} face(s) in this photograph.".format(len(face_locations)))
-        cv2.imsave (str(i) + '.png', img)
-        i = i+1
-    k = cv2.waitKey(10)& 0xff
-    if k == 27:
-        break
-cap.release()
-cv2.destroyAllWindows()"""
 
 import cv2, queue, threading, time
 
@@ -45,39 +30,37 @@ class VideoCapture:
   def read(self):
     return self.q.get()
 
-#cap = VideoCapture('rtsp://admin:thietgia95@192.168.1.245:554/cam/realmonitor?channel=1&subtype=1')
-#cap = VideoCapture('rtsp://admin:thietgia95@192.168.1.247:554/cam/realmonitor?channel=1&subtype=1')
-#cap = VideoCapture('rtsp://admin:thietgia95@192.168.1.248:554/cam/realmonitor?channel=1&subtype=1')
 cap_list = [VideoCapture('rtsp://admin:thietgia95@192.168.1.245:554/cam/realmonitor?channel=1&subtype=1'),
             VideoCapture('rtsp://admin:thietgia95@192.168.1.247:554/cam/realmonitor?channel=1&subtype=1'),
             VideoCapture('rtsp://admin:thietgia95@192.168.1.248:554/cam/realmonitor?channel=1&subtype=1'),
             VideoCapture('rtsp://admin:thietgia95@192.168.1.249:554/cam/realmonitor?channel=1&subtype=1'),
             VideoCapture('rtsp://admin:thietgia95@192.168.1.250:554/cam/realmonitor?channel=1&subtype=1')]
-window_dim = (800, 1200)
+window_dim = (900, 1600)
 cap_list_dim = [(0,0, 800, 800), (0, 800, 200, 400), (200, 800, 200, 400), (400, 800, 200, 400), (600, 800, 200, 400)]
+scale = 100
+cap_list_dim = np.array([(0,0, 100, 66.6), (0, 66.7, 25, 33.3), (25, 66.7, 25, 33.3), (50, 66.7, 25, 33.3), (75, 66.7, 25, 33.3)]).astype(np.float64)
+cap_list_dim[:,0]*= window_dim[0]/scale
+cap_list_dim[:,1]*= window_dim[1]/scale
+cap_list_dim[:,2]*= window_dim[0]/scale
+cap_list_dim[:,3]*= window_dim[1]/scale
+cap_list_dim = cap_list_dim.astype (np.int32)
 og_windows = np.zeros ((*(window_dim), 3), dtype = np.uint8)
 while True:
     for idx, cap in enumerate(cap_list):
       img = cap.read()
-      img = cv2.resize (img, (cap_list_dim[idx][3:1:-1]))
-      #img = cv2.resize (img, (cap_list_dim[idx][2:4]))
-      og_windows [cap_list_dim[idx][0]:(cap_list_dim[idx][0]+cap_list_dim[idx][2]),
-                  cap_list_dim[idx][1]:(cap_list_dim[idx][1]+cap_list_dim[idx][3])] = img
+      cap_dim = cap_list_dim[idx]
+      img = cv2.resize (img, (cap_dim[3:1:-1]))
+      if (cap_dim[0]+cap_dim[2] > og_windows.shape[0]) or (
+        cap_dim[1]+cap_dim[3] > og_windows.shape[1]):
+          og_windows [cap_dim[0]:min(cap_dim[0]+cap_dim[2], og_windows.shape[0]),
+                  cap_dim[1]:min(cap_dim[1]+cap_dim[3], og_windows.shape[1])] = img[0:min(cap_dim[0]+cap_dim[2], og_windows.shape[0])-cap_dim[0],
+                                                                                    0:min(cap_dim[1]+cap_dim[3], og_windows.shape[1])-cap_dim[1]]
+      else:
+          og_windows [cap_dim[0]:(cap_dim[0]+cap_dim[2]),
+                  cap_dim[1]:(cap_dim[1]+cap_dim[3])] = img
     cv2.imshow('video output', og_windows)
     k = cv2.waitKey(10)& 0xff
     if k == 27:
         break
-"""while True:
-    img = cap.read()
-    cv2.imshow('video output', img)
-    #face_locations = face_recognition.face_locations(img)
-    #if (len(face_locations)!=5):
-    #    print("I found {} face(s) in this photograph.".format(len(face_locations)))
-        #cv2.imsave (str(i) + '.png', img)
-    #    i = i+1
-    k = cv2.waitKey(10)& 0xff
-    if k == 27:
-        break
-"""
 cap.release()
 cv2.destroyAllWindows()
